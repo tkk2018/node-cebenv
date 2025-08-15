ba392cd28aa03f6f03f41e88ef9c9c5536001e32
tkk2018 <>
2025-08-15 09:29:21 +0800

    refactor(helper): expose exec options in asyncExec

--- helpers/async-child-process.js
+++ helpers/async-child-process.js
@@ -4,10 +4,11 @@
  * Helper: Run shell commands
  *
  * @param {string} cmd
+ * @param {(import('fs').ObjectEncodingOptions & import('child_process').ExecOptions) | undefined | null} option
  */
-function asyncExec(cmd) {
+function asyncExec(cmd, option) {
   return new Promise((resolve, reject) => {
-    require('child_process').exec(cmd, (err, stdout, stderr) => {
+    require('child_process').exec(cmd, option, (err, stdout, stderr) => {
       if (err) reject(stderr);
       else resolve(stdout);
     });
