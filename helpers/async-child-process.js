eea8493457fe8845b4819db37d5a07176c2ee30a
tkk2018 <>
2025-08-15 10:09:11 +0800

    fix(lint): make exec options parameter optional

--- helpers/async-child-process.js
+++ helpers/async-child-process.js
@@ -4,7 +4,7 @@
  * Helper: Run shell commands
  *
  * @param {string} cmd
- * @param {(import('fs').ObjectEncodingOptions & import('child_process').ExecOptions) | undefined | null} option
+ * @param {(import('fs').ObjectEncodingOptions & import('child_process').ExecOptions) | undefined | null} [option]
  */
 function asyncExec(cmd, option) {
   return new Promise((resolve, reject) => {
